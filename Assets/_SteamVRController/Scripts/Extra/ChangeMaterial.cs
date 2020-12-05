b5973277380da3d00780c2ac7e1107ef551a973a
Andrew Connell <Drew.L.Connell@gmail.com>
2020-12-05 15:20:49 -0500

    Initial Project

---
+++ Assets/_SteamVRController/Scripts/Extra/ChangeMaterial.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    public Material otherMaterial = null;
+
+    private MeshRenderer meshRenderer = null;
+    private Material originalMaterial = null;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        originalMaterial = meshRenderer.material;
+    }
+
+    public void SetOtherMaterial()
+    {
+        meshRenderer.material = otherMaterial;
+    }
+
+    public void SetOriginalMaterial()
+    {
+        meshRenderer.material = originalMaterial;
+    }
+}
---
+++ Assets/_SteamVRController/Scripts/SteamVRBasedController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Valve.VR;
+
+public class SteamVRBasedController : XRBaseController
+{
+    // SteamVR Tracking
+
+    // SteamVR Input
+
+    private void Start()
+    {
+        // Start SteamVR
+    }
+
+    protected override void UpdateTrackingInput(XRControllerState controllerState)
+    {
+        // Get position from pose action
+
+        // Get rotation from position action
+    }
+
+    protected override void UpdateInput(XRControllerState controllerState)
+    {
+        // Reset all of the input values
+
+        // Check select action, apply to controller
+
+        // Check activate action, apply to controller
+
+        // Check UI action, apply to controller
+    }
+
+    private void SetInteractionState(ref InteractionState interactionState, SteamVR_Action_Boolean_Source action)
+    {
+        // Pressed this frame
+
+        // Released this frame
+
+        // Is pressed
+    }
+}
+
